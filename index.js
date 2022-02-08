cb1cd519aee41d3b3f1c982bcfcbf78f22bee20e
matteo9966 <matteo_tommasi96@hotmail.com>
2022-02-08 23:35:37 +0100

    1 file modified

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import { k_combinations } from './index2.js';
-const lista = [{ evento1: 1 }, { evento2: 2 }, { evento3: 3 }];
+const lista = [{ evento: 1 }, { evento: 2 }, { evento: 3 }];
 const combs = k_combinations(lista,2)
+
+const colonnetotali = lista.forEach(array=>{
+  const weight = array.reduce((prev,cur)=>{
+    return ???
+  },0)
+})
+
 
 console.log(combs)
